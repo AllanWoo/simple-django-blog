4ea861d0d97019fb62684979616b54542b77ff27
TonitruiAula <liu_boqian@sina.com>
2016-07-30 16:56:41 +0800

    category template

--- blogsite/blog/urls.py
+++ blogsite/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$',views.index,name='index'),
-	url(r'^(?P<article_id>[0-9]+)/',views.article,name='article')
+	url(r'^(?P<article_id>[0-9]+)/',views.article,name='article'),
+	url(r'^cat=(?P<category_id>[0-9]+)',views.category,name = 'category')
 ]
--- blogsite/blog/views.py
+++ blogsite/blog/views.py
@@ -2,15 +2,17 @@ from django.core.urlresolvers import reverse
 from django.http import Http404, HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
-from .models import Article
+from .models import Article,Category,Tag,Comment
 
 # Create your views here.
 
 def index(request):
 	latest_articles_list = Article.objects.order_by('-date')
+	category_list = Category.objects.order_by('-name')
 	template = loader.get_template('blogs/index.html')
 	context = {
 		'latest_articles_list' : latest_articles_list,
+		'category_list' : category_list,
 	}
 	return render(request,'blogs/index.html',context)
 
@@ -20,3 +22,8 @@ def article(request,article_id):
 	except Article.DoesNotExist:
 		raise Http404("Article does not exist")
 	return render(request, 'blogs/article.html', {'article': article})
+
+def category(request,category_id):
+	cur_cat = Category.objects.get(pk = category_id)
+	articles = Article.objects.filter(category = cur_cat)
+	return render(request, 'blogs/category.html', {'articles': articles})

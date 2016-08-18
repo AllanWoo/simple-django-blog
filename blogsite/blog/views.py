e03a4dc4b68855ab502ddf6326661172c540bab6
TonitruiAula <liu_boqian@sina.com>
2016-08-18 16:09:40 +0800

    add sharing and paging

--- blogsite/blog/views.py
+++ blogsite/blog/views.py
@@ -3,8 +3,11 @@ from django.http import Http404, HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from .models import Article,Category,Tag
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 # Create your views here.
+
+limit = 10;
 
 def index(request):
 	latest_articles_list = Article.objects.order_by('-date')
@@ -29,15 +32,39 @@ def article(request,article_id):
 def category(request,category_id):
 	cur_cat = Category.objects.get(pk = category_id)
 	articles = Article.objects.filter(category = cur_cat)
+	paginator = Paginator(articles,limit)
+	page = request.GET.get('page')
+	try:
+		articles = paginator.page(page)
+	except PageNotAnInteger:
+		articles = paginator.page(1)
+	except EmptyPage:
+		articles = paginator.page(paginator.num_pages)	
 	return render(request, 'blogs/category.html', {'articles': articles,'cat' : cur_cat})
 
 def tag(request,tag_id):
 	cur_tag = Tag.objects.get(pk = tag_id)
 	articles = Article.objects.filter(tags__id__contains = tag_id)
+	paginator = Paginator(articles,limit)
+	page = request.GET.get('page')
+	try:
+		articles = paginator.page(page)
+	except PageNotAnInteger:
+		articles = paginator.page(1)
+	except EmptyPage:
+		articles = paginator.page(paginator.num_pages)	
 	return render(request, 'blogs/tag.html', {'articles': articles,'tag':cur_tag})
 
 def all(request):
 	articles_list = Article.objects.order_by('-date')
+	paginator = Paginator(articles_list,limit)
+	page = request.GET.get('page')
+	try:
+		articles_list = paginator.page(page)
+	except PageNotAnInteger:
+		articles_list = paginator.page(1)
+	except EmptyPage:
+		articles_list = paginator.page(paginator.num_pages)
 	context = {
 		'articles_list' : articles_list,
 	}
